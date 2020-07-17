e422ee4d0d799ea4c348c43489a769be98f94ed9
Abdo <abdelatif.azdoud@gmail.com>
2020-07-17 15:00:47 +0100

    add Location

---
+++ app/src/main/java/com/myguide/libstreaming_test/LastLocationService.java
@@ -0,0 +1,98 @@
+package com.myguide.libstreaming_test;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firestore.v1.WriteResult;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.myguide.libstreaming_test.MainActivity.TAG;
+
+public class LastLocationService extends Service {
+
+    public static String guide = "guide1";
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private DocumentReference LastLocation = db.collection("guides").document(guide);
+    private LocationListener listener;
+    private LocationManager locationManager;
+    private Map<String, Location> Location = new HashMap<>();
+    private Map<String, String> test = new HashMap<>();
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //Toast.makeText(getApplicationContext(),"servise started",Toast.LENGTH_LONG).show();
+        LastLocation.set(test);
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                //last Location
+                Location.put("LastLocation", location);
+                LastLocation.set(Location);
+                Toast.makeText(getApplicationContext(),Location+"",Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        };
+        locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0, listener);
+    }
+}
